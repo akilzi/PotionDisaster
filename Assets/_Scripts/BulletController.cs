cb218c8a227223f15c023f24ba04eaf64d99acb2
Nick Snyder <nickolus451@gmail.com>
2015-01-24 17:51:23 -0600

    merged in movement code with everything else

---
+++ Assets/_Scripts/BulletController.cs
@@ -0,0 +1,44 @@
+﻿using HutongGames.PlayMaker.Actions;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+
+    public Vector3 forceVector;
+    private GameObject _potionLogic;
+
+	void Start ()
+	{
+	    _potionLogic = GameObject.FindGameObjectWithTag("PotionLogic");
+	    PotionCombinations potionCombinations = _potionLogic.GetComponent<PotionCombinations>();
+	    renderer.sharedMaterial.color = potionCombinations.SelectedColor;
+	}
+	
+	void Update () 
+    {
+	    if (gameObject.activeSelf)
+	    {
+	        if (!float.IsNaN(forceVector.x))
+	        {
+                rigidbody2D.AddForce(forceVector);
+	        }
+
+	        forceVector = shortenLength(forceVector, 1000);
+	    }
+    }
+
+    Vector3 shortenLength(Vector3 vector, float reductionLength)
+    {
+        Vector3 b = vector;
+        b *= (1 - reductionLength / vector.magnitude);
+        return b;
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Wall")
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+}
--- Assets/_Scripts/PotionCombinations.cs
+++ Assets/_Scripts/PotionCombinations.cs
@@ -1,147 +1,178 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PotionCombinations : MonoBehaviour {
+public class PotionCombinations : MonoBehaviour
+{
 
-	bool Red = false;
-	bool Yellow = false;
-	bool Blue = false;
-	int numberOfPotionsSelected = 1;
-	string potionA;
-	string potionB;
-	string mixtures;
-	public GameObject mixtureTub;
-	public GameObject Potion_A_Selected;
-	public GameObject Potion_B_Selected;
+    bool Red = false;
+    bool Yellow = false;
+    bool Blue = false;
+    int numberOfPotionsSelected = 1;
+    string potionA;
+    string potionB;
+    string mixtures;
+    public GameObject mixtureTub;
+    public GameObject Potion_A_Selected;
+    public GameObject Potion_B_Selected;
+    public Color SelectedColor;
+    public GameObject Player;
+    private PlayerController _playerController;
 
-	// Use this for initialization
-	void Start () {
-		Debug.Log (numberOfPotionsSelected);
-		mixtureTub.renderer.sharedMaterial.color = Color.white;
-	}
-	
-	// Update is called once per frame
-	void Update() {
+    // Use this for initialization
+    void Start()
+    {
+        _playerController = Player.GetComponent<PlayerController>();
 
-	}
+        Debug.Log(numberOfPotionsSelected);
+        mixtureTub.renderer.sharedMaterial.color = Color.white;
+        SelectedColor = Color.white;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
 
-//RED
+    //RED
 
-	public void redActive ()
-	{
-		Debug.Log (numberOfPotionsSelected);
-		if (numberOfPotionsSelected == 2)
-		{
-			potionB = "Red";
-			Potion_B_Selected.renderer.sharedMaterial.color = Color.red;
-			checkMixtures ();
-			Debug.Log ("mixture complete" + ", " + "potions are:" + " " +  "potion A =" + " " + potionA + ", " + "potion B =" + " " + potionB + " -- " + "mixture is:" + " " + mixtures);
-			numberOfPotionsSelected = 1;
-			potionA = " ";
-			potionB = " ";
-		}
-		else
-		{
-			numberOfPotionsSelected ++;
-			Red = true;
-			potionA = "Red";
-			Potion_A_Selected.renderer.sharedMaterial.color = Color.red;
-			//checkMixtures ();
-		}
-	}
+    public void redActive()
+    {
+        Debug.Log(numberOfPotionsSelected);
+        if (numberOfPotionsSelected == 2)
+        {
+            _playerController.ColorSelected = true;
+            potionB = "Red";
+            Potion_B_Selected.renderer.sharedMaterial.color = Color.red;
+            checkMixtures();
+            Debug.Log("mixture complete" + ", " + "potions are:" + " " + "potion A =" + " " + potionA + ", " + "potion B =" + " " + potionB + " -- " + "mixture is:" + " " + mixtures);
+            numberOfPotionsSelected = 1;
+            potionA = " ";
+            potionB = " ";
+        }
+        else
+        {
+            numberOfPotionsSelected++;
+            Red = true;
+            potionA = "Red";
+            Potion_A_Selected.renderer.sharedMaterial.color = Color.red;
+            //checkMixtures ();
+        }
+    }
 
-//BLUE
+    //BLUE
 
-	public void blueActive ()
-	{
-		Debug.Log (numberOfPotionsSelected);
-		if (numberOfPotionsSelected == 2)
-		{
-			potionB = "Blue";
-			Potion_B_Selected.renderer.sharedMaterial.color = Color.blue;
-			checkMixtures ();
-			Debug.Log ("mixture complete" + ", " + "potions are:" + " " +  "potion A =" + " " + potionA + ", " + "potion B =" + " " + potionB + " -- " + "mixture is:" + " " + mixtures);
-			numberOfPotionsSelected = 1;
-			potionA = " ";
-			potionB = " ";
-		}
-		else
-		{
-			numberOfPotionsSelected ++;
-			Blue = true;
-			potionA = "Blue";
-			Potion_A_Selected.renderer.sharedMaterial.color = Color.blue;
+    public void blueActive()
+    {
+        Debug.Log(numberOfPotionsSelected);
+        if (numberOfPotionsSelected == 2)
+        {
+            _playerController.ColorSelected = true;
+            potionB = "Blue";
+            Potion_B_Selected.renderer.sharedMaterial.color = Color.blue;
+            checkMixtures();
+            Debug.Log("mixture complete" + ", " + "potions are:" + " " + "potion A =" + " " + potionA + ", " + "potion B =" + " " + potionB + " -- " + "mixture is:" + " " + mixtures);
+            numberOfPotionsSelected = 1;
+            potionA = " ";
+            potionB = " ";
+        }
+        else
+        {
+            numberOfPotionsSelected++;
+            Blue = true;
+            potionA = "Blue";
+            Potion_A_Selected.renderer.sharedMaterial.color = Color.blue;
 
-		}
-	}
+        }
+    }
 
-//YELLOW
+    //YELLOW
 
-	public void yellowActive ()
-	{
-		Debug.Log (numberOfPotionsSelected);
-		if (numberOfPotionsSelected == 2) 
-		{
-			potionB = "Yellow";
-			Potion_B_Selected.renderer.sharedMaterial.color = Color.yellow;
-			checkMixtures ();
-			Debug.Log ("mixture complete" + ", " + "potions are:" + " " +  "potion A =" + " " + potionA + ", " + "potion B =" + " " + potionB + " -- " + "mixture is:" + " " + mixtures);
-			numberOfPotionsSelected = 1;
-			potionA = " ";
-			potionB = " ";
-		} 
-		else 
-		{
-			numberOfPotionsSelected ++;
-			Yellow = true;
-			potionA = "Yellow";
-			Potion_A_Selected.renderer.sharedMaterial.color = Color.yellow;
-		
-		}
-	}
+    public void yellowActive()
+    {
+        
+        Debug.Log(numberOfPotionsSelected);
+        if (numberOfPotionsSelected == 2)
+        {
+            _playerController.ColorSelected = true;
+            potionB = "Yellow";
+            Potion_B_Selected.renderer.sharedMaterial.color = Color.yellow;
+            checkMixtures();
+            Debug.Log("mixture complete" + ", " + "potions are:" + " " + "potion A =" + " " + potionA + ", " + "potion B =" + " " + potionB + " -- " + "mixture is:" + " " + mixtures);
+            numberOfPotionsSelected = 1;
+            potionA = " ";
+            potionB = " ";
+        }
+        else
+        {
+            numberOfPotionsSelected++;
+            Yellow = true;
+            potionA = "Yellow";
+            Potion_A_Selected.renderer.sharedMaterial.color = Color.yellow;
 
-//CHECK MIXTURES
+        }
+    }
 
-	void checkMixtures ()
-		{
-		//Debug.Log("Mixture Tested");
-				if (potionA == "Red" && potionB == "Red") {
-						mixtures = "Super Red";
-						mixtureTub.renderer.sharedMaterial.color = Color.red;
-				}
-				if (potionA == "Blue" && potionB == "Blue") {
-						mixtures = "Super Blue";
-						mixtureTub.renderer.sharedMaterial.color = Color.blue;
-				}
-				if (potionA == "Yellow" && potionB == "Yellow") {
-						mixtures = "Super Yellow";
-						mixtureTub.renderer.sharedMaterial.color = Color.yellow;
-				}
-				if (potionA == "Red" && potionB == "Blue") {
-						mixtures = "Purple";
-						mixtureTub.renderer.sharedMaterial.color = Color.magenta;
-				}
-				if (potionA == "Blue" && potionB == "Red") {
-						mixtures = "Purple";
-						mixtureTub.renderer.sharedMaterial.color = Color.magenta;
-				}
-				if (potionA == "Red" && potionB == "Yellow") {
-						mixtures = "Orange";
-						mixtureTub.renderer.sharedMaterial.color = new Color(255, 100, 0, 255);
-				}
-				if (potionA == "Yellow" && potionB == "Red") {
-						mixtures = "Orange";
-						mixtureTub.renderer.sharedMaterial.color = new Color(255, 100, 0, 255);
-				}
-				if (potionA == "Blue" && potionB == "Yellow") {
-						mixtures = "Green";
-						mixtureTub.renderer.sharedMaterial.color = Color.green;
-				}
-				if (potionA == "Yellow" && potionB == "Blue") {
-						mixtures = "Green";
-						mixtureTub.renderer.sharedMaterial.color = Color.green;
-				}
+    //CHECK MIXTURES
 
-		}
+    void checkMixtures()
+    {
+        //Debug.Log("Mixture Tested");
+        if (potionA == "Red" && potionB == "Red")
+        {
+            mixtures = "Super Red";
+            mixtureTub.renderer.sharedMaterial.color = Color.red;
+            SelectedColor = Color.red;
+        }
+        if (potionA == "Blue" && potionB == "Blue")
+        {
+            mixtures = "Super Blue";
+            mixtureTub.renderer.sharedMaterial.color = Color.blue;
+            SelectedColor = Color.blue;
+        }
+        if (potionA == "Yellow" && potionB == "Yellow")
+        {
+            mixtures = "Super Yellow";
+            mixtureTub.renderer.sharedMaterial.color = Color.yellow;
+            SelectedColor = Color.yellow;
+        }
+        if (potionA == "Red" && potionB == "Blue")
+        {
+            mixtures = "Purple";
+            mixtureTub.renderer.sharedMaterial.color = Color.magenta;
+            SelectedColor = Color.magenta;
+        }
+        if (potionA == "Blue" && potionB == "Red")
+        {
+            mixtures = "Purple";
+            mixtureTub.renderer.sharedMaterial.color = Color.magenta;
+            SelectedColor = Color.magenta;
+        }
+        if (potionA == "Red" && potionB == "Yellow")
+        {
+            mixtures = "Orange";
+            mixtureTub.renderer.sharedMaterial.color = new Color(255, 100, 0, 255);
+            SelectedColor = new Color(255, 100, 0, 255);
+        }
+        if (potionA == "Yellow" && potionB == "Red")
+        {
+            mixtures = "Orange";
+            mixtureTub.renderer.sharedMaterial.color = new Color(255, 100, 0, 255);
+            SelectedColor = new Color(255, 100, 0, 255);
+        }
+        if (potionA == "Blue" && potionB == "Yellow")
+        {
+            mixtures = "Green";
+            mixtureTub.renderer.sharedMaterial.color = Color.green;
+            SelectedColor = Color.green;
+        }
+        if (potionA == "Yellow" && potionB == "Blue")
+        {
+            mixtures = "Green";
+            mixtureTub.renderer.sharedMaterial.color = Color.green;
+            SelectedColor = Color.green;
+        }
+
+    }
 }
